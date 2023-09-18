9230b626c3ed4b06bf13c5f25df1d359dfe8aeb5
Alex <workirhin@yandex.ru>
2023-09-18 12:19:08 +0300

    fix error

--- src/components/Todo/Todo.constants.ts
+++ src/components/Todo/Todo.constants.ts
@@ -2,12 +2,12 @@ import type { TList } from "./Todo.types";
 
 export const DEFAULT_LIST: TList = [
   {
-    id: 1,
+    id: '1',
     status: true,
     text: "Do a test task",
   },
   {
-    id: 2,
+    id: '2',
     status: false,
     text: "Find a job",
   },
